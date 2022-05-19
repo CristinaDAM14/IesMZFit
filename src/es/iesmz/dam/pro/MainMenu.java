6a86db30e9070ad9b0c7ccaeaf09d60db5cb4a1b
Miguel Angel Muñoz <mmunoztiscar@gmail.com>
2022-05-19 09:43:43 +0200

    testing branches

--- src/es/iesmz/dam/pro/MainMenu.java
+++ src/es/iesmz/dam/pro/MainMenu.java
@@ -5,6 +5,9 @@ import javax.swing.*;
 public class MainMenu extends JFrame {
 
 
+    private JButton button1;
+    private JPanel panel1;
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(()->{
             JFrame mainMenu = new MainMenu();
--- src/probar.java
+++ src/probar.java
@@ -1,3 +1,7 @@
 public class probar {
+    public static void main(String[] args) {
+
+
+    }
 
 }
